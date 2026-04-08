c782c247bc01a353fafb57635d63000dcb8bd6f1
leylasenicak <les00@edu.zealand.dk>
2026-04-08 10:22:38 +0200

    ny fil - navigation2.php

---
+++ components/navigation2.php
@@ -0,0 +1,40 @@
+
+<div class="navbar">
+    <a href="../index.php" class="nav-item">
+        <i class="icon bi bi-house-door-fill"></i>
+    </a>
+
+   <div class="center-item">
+    <a href="rapport.php" class="center-btn">
+        <i class="icon fa-solid fa-plus" style="color: white;"></i>
+    </a>
+   </div>
+
+    <a href="contact.php" class="nav-item" data-bs-toggle="modal" data-bs-target="#kontaktModal">
+        <i class="icon bi bi-telephone-fill"></i>
+    </a>
+</div>
+
+<div class="modal fade" id="kontaktModal" tabindex="-1" aria-labelledby="kontaktModalLabel">
+    <div class="modal-dialog modal-dialog-centered"> <div class="modal-content custom-modal">
+
+            <div class="modal-header border-0">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body text-start p-4">
+                <div class="mb-4">
+                    <i class="bi bi-telephone kontaktmodalicon"></i>
+                    <span class="ms-3 kontaktmodaltekst">+45 01020304</span>
+                </div>
+                <div>
+                    <i class="bi bi-envelope kontaktmodalicon"></i>
+                    <span class="ms-3 kontaktmodaltekst">kontakt@zealand.dk</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
--- details.php
+++ details.php
@@ -1,1 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/737b386bab.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+</head>
+
+<body style="bg-$white">
+
+
+<div class="details">
+    <div class="headerimg">
+        <img src="img/shop/shop1-netto.jpg" alt="">
+    </div>
+</div>
+
+
+
+
+
+
+<?php include 'components/navigation2.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
